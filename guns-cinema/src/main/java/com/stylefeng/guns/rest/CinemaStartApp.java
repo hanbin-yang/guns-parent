f1a40e8b0ed067eae299db435140a7a77a77d888
Administrator <1217132726@qq.com>
2020-03-12 21:42:48 +0800

    第一次修改

--- guns-cinema/src/main/java/com/stylefeng/guns/rest/CinemaStartApp.java
+++ guns-cinema/src/main/java/com/stylefeng/guns/rest/CinemaStartApp.java
@@ -9,7 +9,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class CinemaStartApp {
 
     public static void main(String[] args) {
-        //哈哈哈哈
+        //哈哈哈哈a
         SpringApplication.run(CinemaStartApp.class, args);
     }
 }
